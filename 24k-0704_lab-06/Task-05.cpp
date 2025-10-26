58b697411c6d39160a3b9a5a30507dece9b80bd5
MuhammadSaim-razzaq <saimrazzaq1103@gmail.com>
2025-10-26 10:00:11 -0700

    Create Task-05.cpp

---
+++ 24k-0704_lab-06/Task-05.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stack>
+#include <string>
+#include <cctype>
+#include <cmath>
+using namespace std;
+
+int main() {
+    string exp;
+    cin >> exp;
+    stack<int> s;
+    for (int i = exp.size() - 1; i >= 0; i--) {
+        char c = exp[i];
+        if (isdigit(c)) s.push(c - '0');
+        else {
+            int a = s.top(); s.pop();
+            int b = s.top(); s.pop();
+            switch (c) {
+                case '+': s.push(a + b); break;
+                case '-': s.push(a - b); break;
+                case '*': s.push(a * b); break;
+                case '/': s.push(a / b); break;
+                case '^': s.push(pow(a, b)); break;
+            }
+        }
+    }
+    cout << s.top();
+}
